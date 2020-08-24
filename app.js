b3817ed32afe97bce5af083d7c260f886049ea88
bane <bane.tepavcevic@gmail.com>
2020-08-24 23:04:37 +0200

    New test methods

--- app.js
+++ app.js
@@ -6,6 +6,28 @@
   LunchCheckFunction.$inject = ["$scope"];
 
   function LunchCheckFunction($scope) {
-    $scope.name = "Bane";
+    //TEST
+    // $scope.count = 0;
+    // $scope.myCheck = function () {
+    //   $scope.count++;
+    // };
+    $scope.string = "";
+    $scope.words = string.split("");
+    $scope.words = string.split("");
+
+    $scope.checkTheFood = function () {
+      // Test clicking method
+      $scope.string = "Bane";
+      if ($scope.string === "") {
+        return "Empty";
+      }
+    };
+
+    $scope.sayMsg = function () {
+      // if ($scope.message > 3) {
+      //   return "Enjoy";
+      // }
+      return "Enjoy!";
+    };
   }
 })();
